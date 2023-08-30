783454f737104edd280234be94e3899829e3a9fc
vikaskumar2500 <vikas.nits8084@gamil.com>
2023-08-31 02:05:16 +0530

    error message showing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,70 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector, useDispatch } from "react-redux";
+import { uiActions } from "./store/uiReducer";
+import Notification from "./components/UI/Notification";
 
 function App() {
-  const showCart = useSelector((state) => state.cart.showCart);
+  const dispatch = useDispatch();
 
+  const showCart = useSelector((state) => state.cart.showCart);
+  const cart = useSelector((state) => state.cart);
+  let notification = useSelector(state=> state.error.notification);
+  useEffect(() => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+    fetch(
+      "https://shoping-page-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+      {
+        method: "PUT",
+        body: JSON.stringify(cart),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then(() => {
+        dispatch(
+          uiActions.showNotification({
+            status: "success",
+            title: "Success!",
+            message: "Send cart data successfully!",
+          })
+        );
+      })
+      .catch(() => {
+        dispatch(
+          uiActions.showNotification({
+            status: "error",
+            title: "Error!",
+            message: "Sending cart data failed!",
+          })
+        );
+      });
+  }, [cart, dispatch]);
+
+  if(notification.status === 'success') {
+    setTimeout(()=> {
+      notification = null;
+    }, 3e3);
+  }
+  console.log(notification);
   return (
     <Layout>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
       {showCart && <Cart />}
       <Products />
     </Layout>
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import cartReducer from "./cartReducer";
+import errorReducer from "./uiReducer";
 
 const store = configureStore({
-  reducer: { cart: cartReducer },
+  reducer: { cart: cartReducer, error: errorReducer },
 });
 
 export default store;
